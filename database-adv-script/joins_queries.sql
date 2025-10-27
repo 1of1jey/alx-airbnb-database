4255bec3e5e36973d3385be31fd5f5bcddf6d712
jeffrey <eshunjeffrey12@gmail.com>
2025-10-27 19:46:02 +0000

    advanced join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -16,4 +16,29 @@ ORDER BY
     b.created_at DESC;
 
 
+-- Enhanced INNER JOIN: Include property details as well
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+    p.property_id,
+    p.name AS property_name,
+    p.pricepernight
+FROM 
+    Booking b
+INNER JOIN 
+    "User" u ON b.user_id = u.user_id
+INNER JOIN 
+    Property p ON b.property_id = p.property_id
+ORDER BY 
+    b.created_at DESC;
+
+
 
