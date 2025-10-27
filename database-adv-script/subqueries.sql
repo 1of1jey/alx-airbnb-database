0dabe9870b0c4daa108af81d82813090d469562f
jeffrey <eshunjeffrey12@gmail.com>
2025-10-27 20:41:36 +0000

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,16 @@
-
+SELECT 
+    p.property_id,
+    p.name,
+    p.pricepernight,
+    p.host_id
+FROM 
+    Property p
+WHERE 
+    p.property_id IN (
+        SELECT r.property_id
+        FROM Review r
+        GROUP BY r.property_id
+        HAVING AVG(r.rating) > 4.0
+    )
+ORDER BY 
+    p.name;
