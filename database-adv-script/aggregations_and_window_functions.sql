1351246b9f718cfc3809fc14e0b51e905416535d
jeffrey <eshunjeffrey12@gmail.com>
2025-10-28 18:01:36 +0000

    Add SQL query for user bookings aggregation

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,15 @@
 
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    "User" u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name, u.email
+ORDER BY 
+    total_bookings DESC, u.first_name;
