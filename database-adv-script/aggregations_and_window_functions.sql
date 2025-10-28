4d30bdbe3a7fe01345491f1f83ddf24d21b582b6
jeffrey <eshunjeffrey12@gmail.com>
2025-10-28 18:06:25 +0000

    Duplicate query for property rankings in SQL script

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,3 +5,11 @@ SELECT
 FROM bookings
 GROUP BY property_id
 ORDER BY property_rank;
+
+SELECT
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+FROM bookings
+GROUP BY property_id
+ORDER BY property_rank;
